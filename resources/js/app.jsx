8be42591aa14d7c9d5b937d946906e318624103e
Andrianjafy23 <lazabe4@gmail.com>
2024-11-20 21:05:47 +0300

    Initial commit pour mon compte

---
+++ resources/js/app.jsx
@@ -0,0 +1,10 @@
+import './bootstrap';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import Head from './components/Head';
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+    <Head/>
+)
---
+++ resources/js/components/Head.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Head() {
+  return (
+    <div>
+        <h2> Bonjour </h2>
+    </div>
+  )
+}
+
+export default Head
